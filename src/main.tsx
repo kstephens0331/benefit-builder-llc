e969fe656f3df984e613eaeedb2d5ff2664db40c
Kyle Stephens <Kyle_stephens31@icloud.com>
2025-08-31 13:30:35 -0500

    updated fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
 import { HelmetProvider } from "react-helmet-async";
+import "@fontsource-variable/inter/index.css";
+import "@fontsource-variable/lora/index.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
